2353b34d7e7ed9fb88f35b9b50de80ebe3056e80
unbreakble26 <38845697+unbreakble26@users.noreply.github.com>
2018-06-01 18:12:05 +0900

    Update convenient_store_pondas.py

--- convenient_store_pondas.py
+++ convenient_store_pondas.py
@@ -47,3 +47,46 @@ print(data.head())
 
 print('-'*45)
 print(data[data.more_than_6000 == True].describe())
+
+#조건 값 주고 Group 화하기
+print('-'*45)
+
+def more_than_6000(x):
+    if x >= 6000:
+        return 'A Group'
+    else:
+        return 'B Group'
+
+data['more_than_6000_2'] = data.hourly_wage.map( lambda x : more_than_6000(x) )
+print( data.head(4) )
+print( type( data['more_than_6000_2']) )
+
+#전국 데이터에서 구 정보 / 시급 데이터만을 추출 하여 시급 기준 내림차순으로 정렬 해보겠습니다.
+data2 = data[data.more_than_6000 == True][ ['area1', 'hourly_wage'] ]
+data2 = data2.sort_values(by='hourly_wage', ascending=0)
+print(data2)
+
+#csv파일로 추출하기 --> 깨짐
+data.to_csv('data_changing.csv', index=False)
+
+#그래프로 그리기
+'''bins = 10 : 구간을 10구간으로 설정.'''
+#data['hourly_wage'].hist(bins=10)
+#matplotlib.pyplot.show()
+
+#boxplot
+#data.boxplot(column='hourly_wage', return_type = 'dict')
+#matplotlib.pyplot.show()
+
+# boxplot 중 name으로 여러개 측정
+# data.boxplot(column='hourly_wage', by='name')
+# matplotlib.pyplot.show()
+
+# boxplot 중 구별로
+# data.boxplot(column='hourly_wage', by='area1')
+# matplotlib.pyplot.show()
+
+# x/y축 변경 y축 폰트 추가
+data.boxplot(column='hourly_wage', by='area1', vert=False)
+matplotlib.pyplot.yticks(fontsize=9) #폰트 사이즈 설정
+matplotlib.pyplot.show()
